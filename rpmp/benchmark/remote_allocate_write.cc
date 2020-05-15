4d1dcd34b48016f08846de83545b263f351222b0
Chendi.Xue <chendi.xue@intel.com>
2020-05-15 08:54:36 +0800

    [RPMP][CPPTESTS] Update Benchmarks
    Signed-off-by: Chendi.Xue <chendi.xue@intel.com>

--- rpmp/benchmark/remote_allocate_write.cc
+++ rpmp/benchmark/remote_allocate_write.cc
@@ -8,8 +8,10 @@
  */
 
 #include <string.h>
-#include <thread>  // NOLINT
 #include <atomic>
+#include <thread>  // NOLINT
+#include "pmpool/Base.h"
+#include "pmpool/Config.h"
 #include "pmpool/client/PmPoolClient.h"
 
 uint64_t timestamp_now() {
@@ -20,7 +22,7 @@ uint64_t timestamp_now() {
 std::atomic<uint64_t> count = {0};
 std::mutex mtx;
 char str[1048576];
-std::vector<PmPoolClient *> clients;
+std::vector<std::shared_ptr<PmPoolClient>> clients;
 std::map<int, std::vector<uint64_t>> addresses;
 
 void func1(int i) {
@@ -43,8 +45,11 @@ void func1(int i) {
   }
 }
 
-int main() {
-  std::vector<std::thread*> threads;
+int main(int argc, char **argv) {
+  /// initialize Config class
+  std::shared_ptr<Config> config = std::make_shared<Config>();
+  CHK_ERR("config init", config->init(argc, argv));
+  std::vector<std::shared_ptr<std::thread>> threads;
   memset(str, '0', 1048576);
 
   int num = 0;
@@ -52,7 +57,8 @@ int main() {
   num = 0;
   count = 0;
   for (int i = 0; i < 4; i++) {
-    PmPoolClient *client = new PmPoolClient("172.168.0.40", "12346");
+    auto client =
+        std::make_shared<PmPoolClient>(config->get_ip(), config->get_port());
     client->begin_tx();
     client->init();
     client->end_tx();
@@ -61,12 +67,11 @@ int main() {
   }
   uint64_t start = timestamp_now();
   for (int i = 0; i < num; i++) {
-    auto t = new std::thread(func1, i);
+    auto t = std::make_shared<std::thread>(func1, i);
     threads.push_back(t);
   }
   for (int i = 0; i < num; i++) {
     threads[i]->join();
-    delete threads[i];
   }
   uint64_t end = timestamp_now();
   std::cout << "pmemkv put test: 1048576 "
@@ -84,7 +89,6 @@ int main() {
   std::cout << "freed." << std::endl;
   for (int i = 0; i < num; i++) {
     clients[i]->wait();
-    delete clients[i];
   }
   return 0;
 }
