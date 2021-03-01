d74cbc0d17e78999a6460df88dc28a1c5ef0c9e5
philo <feilong.he@intel.com>
2021-03-01 19:15:53 +0800

    Refine the start script

--- rpmp/start-rpmp.sh
+++ rpmp/start-rpmp.sh
@@ -1,12 +1,12 @@
 bin=$(pwd)
 
+#Keep this arg for future use.
 USER_VARGS=
 while [ $# != 0 ]; do
   case "$1" in
     "--help" | "-h")
       echo "--help -h Show this usage information"
-      echo "--config Specify or overwrite an configure option."
-      echo "-format Format the configured database."
+      exit
       shift
       ;;
     *)
@@ -18,22 +18,31 @@ done
 
 CONFIG_FILE="./config/rpmp.conf"
 PROXY_ADDR_KEY="rpmp.network.proxy.address"
+SERVER_ADDR_KEY="rpmp.network.server.address"
 PROXY_ADDR=
+SERVER_ADDR=
 
 while IFS= read -r line; do
-    echo "Text read from file: $line"
     tokens=( $line )
     if [ "${tokens[0]}" = "$PROXY_ADDR_KEY" ]; then
       PROXY_ADDR=${tokens[1]}
-      break;
+    elif [ "${tokens[0]}" = "$SERVER_ADDR_KEY" ]; then
+      SERVER_ADDR=${tokens[1]}
     fi
 done < $CONFIG_FILE
 
 #Separate address by ','.
 IFS=','
+#Start RPMP proxy
 for addr in $PROXY_ADDR; do
+  echo "Starting RPMP proxy on $addr.."
   #Pass addr to proxy
-  ssh $addr; cd $bin; ./proxyMain $addr
+  ssh $addr "cd $bin; ./proxyMain $addr"
 done
 
-#TODO: get RPMP server address and launch them one by one.
+#Start RPMP server
+for addr in $SERVER_ADDR; do
+  echo "Starting RPMP server on $addr.."
+  #Pass addr to server
+  ssh $addr "cd $bin; ./main $addr"
+done
